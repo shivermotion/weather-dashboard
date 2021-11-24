3cafa73b2072b8e65b2d90eecd20164311f6795b
Jason Day <jason.day.blue@gmail.com>
2021-11-23 23:56:02 -0600

    passing api data into elements - working

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 let feature = document.getElementById("feature");
-//working api function - need to hook {city} into search bar?
+let currentTemp = document.getElementById("currentTemp");
+let currentHumidity = document.getElementById("currentHumidity");
+let currentWindSpeed = document.getElementById("currentWindSpeed");
+// getOneCall is a working api function when clicking search button - still need to pass {city} input calue from  search bar into API
 function getOneCall() {
 	var requestUrl =
+		//TODO://currently can only search "Dallas". Need to be able to pass an input value into the url for the api to search any city
 		"https://api.openweathermap.org/data/2.5/forecast?q=Dallas&appid=7de4992e2cec27e41e75e10841c8d6f0";
 	fetch(requestUrl)
 		.then(function (response) {
@@ -10,6 +14,16 @@ function getOneCall() {
 		.then(function (data) {
 			console.log(data);
 			feature.textContent = data.city.name;
+			console.log(data.city.name);
+			currentTemp.textContent =
+				"Temperature: " + data.list[0].main.temp;
+			console.log(data.list[0].main.temp);
+			currentHumidity.textContent =
+				"Humidity: " + data.list[0].main.humidity;
+			console.log(data.list[0].main.humidity);
+			currentWindSpeed.textContent =
+				"Wind Speed: " + data.list[0].wind.speed;
+			console.log(data.list[0].wind.speed);
 		});
 }
 
